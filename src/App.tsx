c0dc62bdb1b48421fbee8b158f6b7888653cb9f4
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2024-12-19 22:27:04 -0300

    chore: add tailwind sort plugin and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-green-700">
+    <h1 className="text-3xl font-bold text-green-700 underline">
       Hello world!
     </h1>
   );
