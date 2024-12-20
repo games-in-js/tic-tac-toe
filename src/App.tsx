0d6c0e5968e5c728e7bfa58b5157c7639fcfa746
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2024-12-20 19:40:11 -0300

    chore: customizing tailwind colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <h1 className="text-3xl font-bold text-green-700 underline">
+    <h1 className="text-pink bg-blue-100 text-3xl font-bold underline">
       Hello world!
     </h1>
   );
