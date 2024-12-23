a2e13b3044fbe92525ea29ca5908bfc220a7e69b
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2024-12-23 19:40:16 -0300

    chore: start layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { Gamepad2 } from "lucide-react";
+
 function App() {
   return (
-    <h1 className="text-pink bg-blue-100 text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-blue-200">
+      <div className="w-full max-w-md rounded-2xl bg-blue-100 p-8">
+        <div className="mb-8 flex items-center justify-center gap-3">
+          <Gamepad2 className="h-8 w-8 text-pink" />
+          <h1 className="text-4xl font-bold text-white">Tic Tac Toe</h1>
+        </div>
+      </div>
+    </main>
   );
 }
 
